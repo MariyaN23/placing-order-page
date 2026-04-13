58fabb9d4adf5e4670d340d4c708bb367df03124
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2026-04-13 15:18:03 +0300

    input type range

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import "./style.scss";
+
+const percentOutput = document.querySelector("#percent-output")
+const percentInput = document.querySelector("#percent")
+percentOutput.textContent = `${percentInput.value} %`
+percentInput.addEventListener("input", (event) => {
+    percentOutput.textContent = `${event.target.value} %`
+})
