bd783d428a355baeb8fdf69f39cd63141fe514b6
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2026-04-14 09:59:16 +0300

    update input file

--- scripts/form.js
+++ scripts/form.js
@@ -4,6 +4,7 @@ document.getElementById('orderForm').addEventListener('submit', function (e) {
     e.preventDefault()
     const formData = new FormData(this)
     const data = Object.fromEntries(formData.entries())
+    // send data
     alert('Форма отправлена')
     this.reset()
     resetSelect()
--- scripts/input.js
+++ scripts/input.js
@@ -9,9 +9,20 @@ percentInput.addEventListener("input", (event) => {
 //input type file
 const wrapper = document.getElementById('fileWrapper')
 const fileInput = document.getElementById('attachment')
+const fileText = document.getElementById('file-text')
 wrapper.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
         e.preventDefault()
         fileInput.click()
+    }
+})
+
+fileInput.addEventListener('change', () => {
+    if (fileInput.files.length > 0) {
+        fileText.textContent = `Файл: ${fileInput.files[0].name}`
+        wrapper.classList.add('has-file')
+    } else {
+        fileText.textContent = initialText;
+        wrapper.classList.remove('has-file')
     }
 })
